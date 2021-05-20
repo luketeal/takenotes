4506105f8fc84b2380a9e533d17931fd038ad5f0
Luke Teal <l.teal21@gmail.com>
2021-05-20 17:14:33 -0400

    add get for apiroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,15 @@
 const { v4: uuidv4 } = require('uuid');
 const db = require('../db/db.json')
 const fs = require('fs')
+
+module.exports = (app) => {
+    // API GET Requests
+  
+    app.get('/api/notes', (req, res) => res.json(db));
+
+    // app.post('/api/notes', (req, res) => {
+    //     const newNoteObj = req.body
+    //     console.log(newNoteObj)
+    // })
+   
+  };
