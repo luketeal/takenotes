eb8fecbc8ee692d45914514ace88e0a7493ad507
Luke Teal <l.teal21@gmail.com>
2021-05-20 16:12:54 -0400

    add html routes

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static(__dirname + '/public'))
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 
 // require('./routes/apiRoutes')(app);
-// require('./routes/htmlRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 // LISTENER
 // The below code effectively "starts" our server
