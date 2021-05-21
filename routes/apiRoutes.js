71c6b1d1f409ce3f1ba867a6ba9cbf2a07d6c4c8
Luke Teal <l.teal21@gmail.com>
2021-05-20 23:35:36 -0400

    remove unnecesasary console logs

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,22 +8,17 @@ module.exports = (app) => {
     app.get('/api/notes', (req, res) => res.json(db));
 
     app.post('/api/notes', (req, res) => {
-        console.log(db)
         const newNoteObj = req.body
         newNoteObj.id = uuidv4()
         db.push(newNoteObj)
-        console.log(db)
         res.json(db)
         fs.writeFile('./db/db.json', JSON.stringify(db), err => { if (err) throw err })
     })
 
     app.delete('/api/notes/:id', (req,res) => {
-        console.log(req.params.id)
-        console.log(db)
         for(each of db) {
             if(each.id === req.params.id) {
                 db.splice(each,1)
-                console.log(db)
                 res.json(db)
                 fs.writeFile('./db/db.json', JSON.stringify(db), err => { if (err) throw err })
             }
